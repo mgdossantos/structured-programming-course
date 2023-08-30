756a2ff45cb0da19e1df19b29aee88fe4c429b49
Marcela Gonçalves dos Santos <marcela.santos1@uqac.ca>
2023-08-30 18:53:25 -0400

    Variables

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        int inStock = 25;
+        double price = 25.37;
+        char size = 'S';
+        bool available = true;
+
+        Console.WriteLine(inStock + " - " + price + " - " + size + " - " + available);
+    }
+
+}
