2971ba4c96cb9e83fab2b46011bd0c4481f84bc2
Marcela Gonçalves dos Santos <marcela.santos1@uqac.ca>
2023-09-06 10:33:17 -0400

    class 3 - update
    some corrections and add the challenge code

--- ExampleTicketChildrenAge/ExampleTicketChildrenAge/Program.cs
+++ ExampleTicketChildrenAge/ExampleTicketChildrenAge/Program.cs
@@ -4,29 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
+        string ageInitial;
         Console.WriteLine("Welcome to Lasalle Theater!!");
         Console.WriteLine("Type your age: ");
-        int age;
-        age = int.Parse(Console.ReadLine());
-        double ticketprice;
+        ageInitial= Console.ReadLine();
+        bool result;
+        result = char.IsDigit(ageInitial[0]);
+        if (result == true)
+        {
+            int age;
+            age = int.Parse(ageInitial);
+            double ticketprice;
 
-        if (age >= 0 || age <= 12)
-        {
-            ticketprice = 5;
-        }
-        else if(age > 12 || age <= 17)
-        {
-            ticketprice = 7.50;
-        }
-        else if(age >17 || age <= 64)
-        {
-            ticketprice = 10;
+            if (age >= 0 && age <= 12)
+            {
+                ticketprice = 5;
+            }
+            else if (age > 12 && age <= 17)
+            {
+                ticketprice = 7.50;
+            }
+            else if (age > 17 && age <= 64)
+            {
+                ticketprice = 10;
+            }
+            else
+            {
+                ticketprice = 6.50;
+            }
+
+            Console.WriteLine($"Your ticket price is: ${ticketprice:F2}");
+
         }
         else
         {
-            ticketprice = 6.50;
+            Console.WriteLine("You did not type valid input!!");
         }
-
-        Console.WriteLine($"Your ticket price is: ${ticketprice:F2}");
     }
 }
