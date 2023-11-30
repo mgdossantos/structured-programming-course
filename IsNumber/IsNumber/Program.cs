f8252daaecf6d4493066fb8e4ead8d0820c74acf
Marcela Gonçalves dos Santos <marcela.santos1@uqac.ca>
2023-11-30 16:38:31 -0500

    IsDigit

---
+++ IsNumber/IsNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Type a year");
+        string year= Console.ReadLine();
+
+        while(!Char.IsNumber(year,0) || !Char.IsNumber(year, 1) || !Char.IsNumber(year, 2) || !Char.IsNumber(year, 3)){
+            Console.WriteLine("Type a year with 4 digits. Please");
+            year = Console.ReadLine();
+        }
+        
+        int yearNumber = int.Parse(year);
+            
+    }
+
+}
